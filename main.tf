50fe6d7d14ddd3933b08304a8f7961b419fcdef5
Ed <edtan@users.noreply.github.com>
2020-04-10 16:10:30 -0400

    * Ran terraform 0.12upgrade * Change AWS to use personal AWS profile

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 /*
 Copyright (c) 2016, UPMC Enterprises
 All rights reserved.
@@ -25,149 +24,145 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 */
 
 provider "aws" {
-  version    = "~> 1.48"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  profile = var.aws_profile
+  region  = var.region
 }
 
 provider "template" {
-  version    = "1.0.0"
 }
 
 provider "random" {
-  version    = "2.0.0"
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
-  tags {
-    Name = "${var.cluster-name}"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = var.cluster-name
+    Environment = var.cluster-name
   }
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags {
-    Name = "${var.cluster-name}"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = var.cluster-name
+    Environment = var.cluster-name
   }
 }
 
 resource "aws_route_table" "r" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    gateway_id = aws_internet_gateway.gw.id
   }
 
-  depends_on = ["aws_internet_gateway.gw"]
+  depends_on = [aws_internet_gateway.gw]
 
-  tags {
-    Name = "${var.cluster-name}"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = var.cluster-name
+    Environment = var.cluster-name
   }
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id = "${aws_subnet.public.id}"
-  route_table_id = "${aws_route_table.r.id}"
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.r.id
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.main.id}"
-  cidr_block = "10.0.100.0/24"
-  availability_zone = "${var.region}${var.az}"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.100.0/24"
+  availability_zone       = "${var.region}${var.az}"
   map_public_ip_on_launch = true
 
-  tags {
-    Name = "${var.cluster-name}"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = var.cluster-name
+    Environment = var.cluster-name
   }
 }
 
 resource "aws_security_group" "kubernetes" {
-  name = "${var.cluster-name}"
+  name        = var.cluster-name
   description = "Allow inbound ssh traffic"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id      = aws_vpc.main.id
 
-  tags {
-    Name = "${var.cluster-name}"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = var.cluster-name
+    Environment = var.cluster-name
   }
 }
 
 resource "aws_security_group_rule" "allow_all_from_self" {
-  type            = "ingress"
-  from_port       = 0
-  to_port         = 0
-  protocol        = "-1"
-  source_security_group_id = "${aws_security_group.kubernetes.id}"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes.id
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_security_group_rule" "allow_ssh_from_admin" {
-  type            = "ingress"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  cidr_blocks     = "${split(",", var.admin-cidr-blocks)}"
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = split(",", var.admin-cidr-blocks)
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_security_group_rule" "allow_k8s_from_admin" {
-  type            = "ingress"
-  from_port       = 6443
-  to_port         = 6443
-  protocol        = "tcp"
-  cidr_blocks     = "${split(",", var.admin-cidr-blocks)}"
+  type        = "ingress"
+  from_port   = 6443
+  to_port     = 6443
+  protocol    = "tcp"
+  cidr_blocks = split(",", var.admin-cidr-blocks)
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_security_group_rule" "allow_https_from_web" {
-  type            = "ingress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_security_group_rule" "allow_http_from_web" {
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 80
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_security_group_rule" "allow_all_out" {
-  type            = "egress"
-  from_port       = 0
-  to_port         = 0
-  protocol        = "-1"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.kubernetes.id}"
+  security_group_id = aws_security_group.kubernetes.id
 }
 
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket_prefix   = "${var.cluster-name}"
-  force_destroy   = "true"
+  bucket_prefix = var.cluster-name
+  force_destroy = "true"
 
-  tags {
-    Environment = "${var.cluster-name}"
+  tags = {
+    Environment = var.cluster-name
   }
 
   lifecycle_rule {
@@ -182,93 +177,98 @@ resource "aws_s3_bucket" "s3-bucket" {
 }
 
 resource "aws_s3_bucket_object" "external-dns-manifest" {
-  count  = "${var.external-dns-enabled}"
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
+  count  = var.external-dns-enabled
+  bucket = aws_s3_bucket.s3-bucket.id
   key    = "manifests/external-dns.yaml"
   source = "manifests/external-dns.yaml"
-  etag   = "${md5(file("manifests/external-dns.yaml"))}"
+  etag   = filemd5("manifests/external-dns.yaml")
 }
 
 resource "aws_s3_bucket_object" "ebs-storage-class-manifest" {
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
+  bucket = aws_s3_bucket.s3-bucket.id
   key    = "manifests/ebs-storage-class.yaml"
   source = "manifests/ebs-storage-class.yaml"
-  etag   = "${md5(file("manifests/ebs-storage-class.yaml"))}"
+  etag   = filemd5("manifests/ebs-storage-class.yaml")
 }
 
 resource "aws_s3_bucket_object" "nginx-ingress-manifest" {
-  count  = "${var.nginx-ingress-enabled}"
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
+  count  = var.nginx-ingress-enabled
+  bucket = aws_s3_bucket.s3-bucket.id
   key    = "manifests/nginx-ingress-mandatory.yaml"
   source = "manifests/nginx-ingress-mandatory.yaml"
-  etag   = "${md5(file("manifests/nginx-ingress-mandatory.yaml"))}"
+  etag   = filemd5("manifests/nginx-ingress-mandatory.yaml")
 }
 
 data "template_file" "nginx-ingress-nodeport-manifest" {
-  count  = "${var.nginx-ingress-enabled}"
-  template = "${file("manifests/nginx-ingress-nodeport.yaml.tmpl")}"
-  vars {
-    nginx_ingress_domain = "${var.nginx-ingress-domain}"
+  count    = var.nginx-ingress-enabled
+  template = file("manifests/nginx-ingress-nodeport.yaml.tmpl")
+  vars = {
+    nginx_ingress_domain = var.nginx-ingress-domain
   }
 }
+
 resource "aws_s3_bucket_object" "nginx-ingress-nodeport-manifest" {
-  count  = "${var.nginx-ingress-enabled}"
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
-  key    = "manifests/nginx-ingress-nodeport.yaml"
-  content = "${data.template_file.nginx-ingress-nodeport-manifest.rendered}"
-  etag   = "${md5(data.template_file.nginx-ingress-nodeport-manifest.rendered)}"
+  count   = var.nginx-ingress-enabled
+  bucket  = aws_s3_bucket.s3-bucket.id
+  key     = "manifests/nginx-ingress-nodeport.yaml"
+  content = data.template_file.nginx-ingress-nodeport-manifest[0].rendered
+  etag = md5(
+    data.template_file.nginx-ingress-nodeport-manifest[0].rendered,
+  )
 }
 
 data "template_file" "cluster-autoscaler-manifest" {
-  template = "${file("manifests/cluster-autoscaler-autodiscover.yaml.tmpl")}"
-  vars {
-    cluster_name = "${var.cluster-name}"
-    cluster_region = "${var.region}"
+  template = file("manifests/cluster-autoscaler-autodiscover.yaml.tmpl")
+  vars = {
+    cluster_name   = var.cluster-name
+    cluster_region = var.region
   }
 }
+
 resource "aws_s3_bucket_object" "cluster-autoscaler-manifest" {
-  count  = "${var.cluster-autoscaler-enabled}"
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
-  key    = "manifests/cluster-autoscaler-autodiscover.yaml"
-  content = "${data.template_file.cluster-autoscaler-manifest.rendered}"
-  etag   = "${md5(data.template_file.cluster-autoscaler-manifest.rendered)}"
+  count   = var.cluster-autoscaler-enabled
+  bucket  = aws_s3_bucket.s3-bucket.id
+  key     = "manifests/cluster-autoscaler-autodiscover.yaml"
+  content = data.template_file.cluster-autoscaler-manifest.rendered
+  etag    = md5(data.template_file.cluster-autoscaler-manifest.rendered)
 }
 
 data "template_file" "cert-manager-issuer-manifest" {
-  template = "${file("manifests/cert-manager-issuer.yaml.tmpl")}"
-  vars {
-    cert_manager_email = "${var.cert-manager-email}"
+  template = file("manifests/cert-manager-issuer.yaml.tmpl")
+  vars = {
+    cert_manager_email = var.cert-manager-email
   }
 }
+
 resource "aws_s3_bucket_object" "cert-manager-issuer-manifest" {
-  count  = "${var.cert-manager-enabled}"
-  bucket = "${aws_s3_bucket.s3-bucket.id}"
-  key    = "manifests/cert-manager-issuer.yaml"
-  content = "${data.template_file.cert-manager-issuer-manifest.rendered}"
-  etag   = "${md5(data.template_file.cert-manager-issuer-manifest.rendered)}"
+  count   = var.cert-manager-enabled
+  bucket  = aws_s3_bucket.s3-bucket.id
+  key     = "manifests/cert-manager-issuer.yaml"
+  content = data.template_file.cert-manager-issuer-manifest.rendered
+  etag    = md5(data.template_file.cert-manager-issuer-manifest.rendered)
 }
 
 data "template_file" "master-userdata" {
-  template = "${file("master.sh")}"
+  template = file("master.sh")
 
-  vars {
-    k8stoken = "${local.k8stoken}"
-    clustername = "${var.cluster-name}"
-    s3bucket = "${aws_s3_bucket.s3-bucket.id}"
-    backupcron = "${var.backup-cron-expression}"
-    k8sversion = "${var.kubernetes-version}"
-    backupenabled = "${var.backup-enabled}"
-    certmanagerenabled = "${var.cert-manager-enabled}"
+  vars = {
+    k8stoken           = local.k8stoken
+    clustername        = var.cluster-name
+    s3bucket           = aws_s3_bucket.s3-bucket.id
+    backupcron         = var.backup-cron-expression
+    k8sversion         = var.kubernetes-version
+    backupenabled      = var.backup-enabled
+    certmanagerenabled = var.cert-manager-enabled
   }
 }
 
 data "template_file" "worker-userdata" {
-  template = "${file("worker.sh")}"
+  template = file("worker.sh")
 
-  vars {
-    k8stoken = "${local.k8stoken}"
-    masterIP = "10.0.100.4"
-    k8sversion = "${var.kubernetes-version}"
+  vars = {
+    k8stoken   = local.k8stoken
+    masterIP   = "10.0.100.4"
+    k8sversion = var.kubernetes-version
   }
 }
 
@@ -300,10 +300,11 @@ resource "aws_iam_role" "role" {
     ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy_attachment" "policy" {
-  role       = "${aws_iam_role.role.name}"
+  role       = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
@@ -339,10 +340,11 @@ resource "aws_iam_policy" "cluster-policy" {
     ]
 }
 EOF
+
 }
 
 resource "aws_iam_policy" "autoscaling" {
-  count       = "${var.cluster-autoscaler-enabled}"
+  count       = var.cluster-autoscaler-enabled
   name        = "${var.cluster-name}-autoscaling-policy"
   path        = "/"
   description = "Policy for ${var.cluster-name} cluster to allow cluster autoscaling to work"
@@ -366,17 +368,18 @@ resource "aws_iam_policy" "autoscaling" {
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy_attachment" "autoscaling" {
-  count      = "${var.cluster-autoscaler-enabled}"
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.autoscaling.arn}"
+  count      = var.cluster-autoscaler-enabled
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.autoscaling[0].arn
 }
 
 resource "aws_iam_role_policy_attachment" "cluster-policy" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.cluster-policy.arn}"
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.cluster-policy.arn
 }
 
 resource "aws_iam_policy" "s3-bucket-policy" {
@@ -405,15 +408,16 @@ resource "aws_iam_policy" "s3-bucket-policy" {
     ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy_attachment" "s3-bucket-policy" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.s3-bucket-policy.arn}"
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.s3-bucket-policy.arn
 }
 
 resource "aws_iam_policy" "route53-policy" {
-  count       = "${var.external-dns-enabled}"
+  count       = var.external-dns-enabled
   name        = "${var.cluster-name}-route53-policy"
   path        = "/"
   description = "Polcy for ${var.cluster-name} cluster to allow access to Route 53 for DNS record creation"
@@ -430,73 +434,74 @@ resource "aws_iam_policy" "route53-policy" {
     ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy_attachment" "route53-policy" {
-  count      = "${var.external-dns-enabled}"
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.route53-policy.arn}"
+  count      = var.external-dns-enabled
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.route53-policy[0].arn
 }
 
 resource "aws_iam_instance_profile" "profile" {
   name = "${var.cluster-name}-instance-profile"
-  role = "${aws_iam_role.role.name}"
+  role = aws_iam_role.role.name
 }
 
 resource "aws_spot_instance_request" "master" {
-  ami           = "${data.aws_ami.latest_ami.id}"
-  instance_type = "${var.master-instance-type}"
-  subnet_id = "${aws_subnet.public.id}"
-  user_data = "${data.template_file.master-userdata.rendered}"
-  key_name = "${var.k8s-ssh-key}"
-  iam_instance_profile   = "${aws_iam_instance_profile.profile.name}"
-  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
-  spot_price = "${var.master-spot-price}"
-  valid_until = "9999-12-25T12:00:00Z"
-  wait_for_fulfillment = true
-  private_ip = "10.0.100.4"
+  ami                    = data.aws_ami.latest_ami.id
+  instance_type          = var.master-instance-type
+  subnet_id              = aws_subnet.public.id
+  user_data              = data.template_file.master-userdata.rendered
+  key_name               = var.k8s-ssh-key
+  iam_instance_profile   = aws_iam_instance_profile.profile.name
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
+  spot_price             = var.master-spot-price
+  valid_until            = "9999-12-25T12:00:00Z"
+  wait_for_fulfillment   = true
+  private_ip             = "10.0.100.4"
 
-  depends_on = ["aws_internet_gateway.gw"]
+  depends_on = [aws_internet_gateway.gw]
 
-  tags {
-    Name = "${var.cluster-name}-master"
-    Environment = "${var.cluster-name}"
+  tags = {
+    Name        = "${var.cluster-name}-master"
+    Environment = var.cluster-name
   }
 
   lifecycle {
-    ignore_changes = [
-      "ami"
-    ]
+    ignore_changes = [ami]
   }
 }
 
 # Spot ASG for workers
 resource "aws_launch_template" "worker" {
-  iam_instance_profile        = { name = "${aws_iam_instance_profile.profile.name}" }
-  image_id                    = "${data.aws_ami.latest_ami.id}"
-  name                        = "${var.cluster-name}-worker"
-  vpc_security_group_ids      = ["${aws_security_group.kubernetes.id}"]
-  key_name                    = "${var.k8s-ssh-key}"
-  instance_type               = "${var.worker-instance-type}"
-  user_data                   = "${base64encode(data.template_file.worker-userdata.rendered)}"
-  ebs_optimized               = false
+  iam_instance_profile {
+    name = aws_iam_instance_profile.profile.name
+  }
+  image_id               = data.aws_ami.latest_ami.id
+  name                   = "${var.cluster-name}-worker"
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
+  key_name               = var.k8s-ssh-key
+  instance_type          = var.worker-instance-type
+  user_data              = base64encode(data.template_file.worker-userdata.rendered)
+  ebs_optimized          = false
   instance_market_options {
     market_type = "spot"
     spot_options {
-      max_price = "${var.worker-spot-price}"
+      max_price = var.worker-spot-price
     }
   }
 }
 
 resource "aws_autoscaling_group" "worker" {
-  max_size             = "${var.max-worker-count}"
-  min_size             = "${var.min-worker-count}"
-  name                 = "${var.cluster-name}-worker"
-  vpc_zone_identifier  = ["${aws_subnet.public.id}"]
+  max_size            = var.max-worker-count
+  min_size            = var.min-worker-count
+  name                = "${var.cluster-name}-worker"
+  vpc_zone_identifier = [aws_subnet.public.id]
 
   launch_template {
-    id = "${aws_launch_template.worker.id}"
-    version = "$$Latest"
+    id      = aws_launch_template.worker.id
+    version = "$Latest"
   }
 
   tag {
@@ -507,7 +512,7 @@ resource "aws_autoscaling_group" "worker" {
 
   tag {
     key                 = "Environment"
-    value               = "${var.cluster-name}"
+    value               = var.cluster-name
     propagate_at_launch = true
   }
 
